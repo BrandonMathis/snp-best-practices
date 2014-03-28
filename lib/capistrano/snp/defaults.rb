7374279d24a319b06f93bcff95e72b171efd49fc
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-03-29 01:47:48 +0400

    Updated defaults Capistrano settings

--- lib/capistrano/snp/defaults.rb
+++ lib/capistrano/snp/defaults.rb
@@ -1,18 +1,20 @@
 namespace :load do
   task :defaults do
-    # Basic settings
-    set :repo_url, -> { "git@git.snpdev.ru:saltpepper/#{fetch(:application)}-api.git" }
+    # config valid only for Capistrano 3.1
+    lock '3.1.0'
+
+    # basic settings
     set :deploy_to, -> { "/var/www/#{fetch(:application)}/ss" }
     set :scm, :git
     set :keep_releases, 3
 
-    # Linked files
+    # linked files
     set :linked_files, %w{config/database.yml}
 
-    # Linked dirs
+    # linked dirs
     set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-    # ENV
+    # environment
     set :default_env, -> { {worker_count: 2, deploy_to: fetch(:deploy_to)} }
   end
 end
