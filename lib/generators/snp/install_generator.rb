60279ac3cfe7d5fc025e5260b422461a1db54b7b
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-03-31 00:21:59 +0400

    Added snp:install generator, which replaces `cap install`

---
+++ lib/generators/snp/install_generator.rb
@@ -0,0 +1,11 @@
+module Snp
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+
+    desc 'This generator substitutes `cap install` adding S&P specific deployment settings'
+    def add_capistrano_templates
+      directory '../templates', '.'
+    end
+  end
+end
+
