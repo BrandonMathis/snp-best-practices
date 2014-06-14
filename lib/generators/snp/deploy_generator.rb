326d2d240175eef125f492601f317dc8aeef849d
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-06-15 00:57:43 +0400

    Add optional creating custom deploy stages

--- lib/generators/snp/deploy_generator.rb
+++ lib/generators/snp/deploy_generator.rb
@@ -1,10 +1,17 @@
 module Snp
   class DeployGenerator < Rails::Generators::Base
+    desc 'Prepares application to deploy and add custom stages. `testing` and `production` stages are created by default.'
+    argument :stages, type: :array, default: [], banner: 'custom stages'
     source_root File.expand_path('../templates', __FILE__)
 
-    desc 'Prepares application to deploy and add custom stages. Defaults are `testing` and `production`.'
     def add_deploy_templates
       directory 'deploy', '.'
+    end
+
+    def create_custom_stages
+      stages.each do |stage|
+        template 'deploy/config/deploy/testing.rb.tt', "config/deploy/#{stage}.rb"
+      end
     end
   end
 end
