f7357d9d6757b226f37c0bb5a4dee25d2c44b6a5
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-06-14 10:41:46 +0400

    Add .editorconfig to snp:install generator

--- lib/generators/snp/install_generator.rb
+++ lib/generators/snp/install_generator.rb
@@ -2,7 +2,7 @@ module Snp
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
-    desc 'This generator substitutes `cap install` adding S&P specific deployment settings'
+    desc 'This generator substitutes `cap install` adding S&P specific project settings'
     def add_capistrano_templates
       directory '../templates', '.'
     end
