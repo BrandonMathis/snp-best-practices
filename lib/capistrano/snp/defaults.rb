c3a6af99f1798f8bc9bb0e3751d43fa7bf104f43
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-10-21 14:44:32 +0400

    Fix #12, linked_files and linked_dirs now are not overrided in defaults

--- lib/capistrano/snp/defaults.rb
+++ lib/capistrano/snp/defaults.rb
@@ -12,20 +12,20 @@ namespace :load do
     set :password, -> { ask('password', nil) }
 
     # linked files
-    set :linked_files, %w(config/database.yml)
+    set :linked_files, fetch(:linked_dirs, []) + %w(config/database.yml)
 
     # linked dirs
-    set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
+    set :linked_dirs, fetch(:linked_dirs, []) + %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 
     # custom environment
-    set :default_env, -> { { worker_count: 2, deploy_to: fetch(:deploy_to) }.merge(fetch(:env) || Hash.new) }
+    set :default_env, -> { { worker_count: 2, deploy_to: fetch(:deploy_to) }.merge(fetch(:env, {})) }
 
     # capistrano-bundler settings
     set :bundle_binstubs, nil
 
     # capistrano-rbenv settings
     set :rbenv_type, :user
-    set :rbenv_ruby, '2.1.0'
+    set :rbenv_ruby, '2.1.2'
 
     # capistrano3-unicorn settings
     set :unicorn_pid, -> { "#{fetch(:deploy_to)}/shared/tmp/pids/unicorn.pid" }
